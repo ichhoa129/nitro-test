881d55feab8b83c653dc4be5021272ed676e2325
hoa.nguyen_ich <ichhoa129@gmail.com>
2022-12-21 16:22:46 +0700

    s

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
+const dayjs = require('dayjs');
+
 console.log("app started")
 
 const interval = setInterval(function() {
    console.log("hello world")
+    console.log(dayjs().format('YYYY-MM-DD HH:mm:ss'))
  }, 5000);
