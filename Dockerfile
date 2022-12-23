ab7661d873feb07458521e0a4507cd4bccd31d12
hoa.nguyen_ich <ichhoa129@gmail.com>
2022-12-23 10:58:36 +0700

    update

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM node:16
+FROM public.ecr.aws/amazonlinux/amazonlinux:2
+
+RUN yum install python3 iproute   -y
 
 WORKDIR /app
 
 COPY . .
-EXPOSE 3000
 
 RUN chmod +x /app/script.sh
 
--- script.sh
+++ script.sh
@@ -1,3 +1,9 @@
-cd "/app/" && npm start
-echo "app stopped"
-sleep 10
+#!/bin/sh
+
+ip addr add 127.0.0.1/32 dev lo &&
+ip link set dev lo up &&
+echo "127.0.0.1   ip-ranges.amazonaws.com" >> /etc/hosts &&
+touch /app/libnsm.so &&
+cd "/app/" &&
+python3 traffic_forwarder.py 127.0.0.1 443 3 8001 &&
+npm start
